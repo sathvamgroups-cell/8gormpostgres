b0cc17f11a380ea80fc4b61742260ceafa57eb81
sathvamgroups-cell <sathvamgroups@gmail.com>
2026-09-17 16:09:06 +0530

    Create database.go

---
+++ database.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func connectDatabase() *gorm.DB {
+
+	dsn := "host=172.29.139.206 user=postgres password=postgres@123 dbname=learnapis port=5432 sslmode=disable"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+
+	fmt.Println("Database connected successfully!")
+
+	return db
+}
